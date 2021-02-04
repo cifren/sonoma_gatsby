8b1d87bd07be87e9d87279581df52b52ba9e2c24
francis <cifren@msn.com>
2021-02-04 11:08:18 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,5 @@
 npm run build
+Remove-Item -Recurse -ErrorAction Ignore -Force .\publicShared-tmp
+Copy-Item -Recurse .\public\ .\publicShared-tmp
+Remove-Item -Recurse -ErrorAction Ignore -Force .\publicShared
+Move-Item .\publicShared-tmp .\publicShared
